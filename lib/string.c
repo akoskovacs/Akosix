69c0c3e451ddb657ac0a0289b912a2f216a842fc
Ákos Kovács <akoskovacs@gmx.com>
2012-03-02 20:31:33 +0100

    Added strncpy() with some NULL pointer checking to the other functions

--- lib/string.c
+++ lib/string.c
@@ -1,18 +1,45 @@
+#include <basic.h>
 #include <string.h>
 
 char *strcpy(char *dest, const char *src)
 {
     char *ret = dest;
+
+    if (src == NULL || dest == NULL)
+        return NULL;
+
     while ((*dest++ = *src++))
         ;
+
     return ret;
 }
 
 size_t strlen(const char *str)
 {
     size_t str_size = 0;
+    if (str == NULL)
+        return 0;
+
     while ((*str++))
         str_size++;
 
     return str_size;
+}
+
+char *strncpy(char *dest, char *src, size_t dest_size)
+{
+    size_t i;
+    if (src == NULL || dest == NULL)
+        return NULL;
+
+    for (i = 0; i < dest_size || *src == NULL; i++) {
+        dest[i] = src[i];
+    }
+
+    while (i < dest_size) {
+        dest[i] = '\0';
+        i++;
+    }
+
+    return i;
 }
