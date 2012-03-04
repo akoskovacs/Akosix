287a073517449a69abe297a8a0501ea39947c5ff
Ákos Kovács <akoskovacs@gmx.com>
2012-03-04 01:24:14 +0100

    Added new string functions.

--- lib/include/string.h
+++ lib/include/string.h
@@ -5,5 +5,8 @@
 
 char *strcpy(char *, const char *);
 size_t strlen(const char *);
+char *strncpy(char *, const char *, size_t);
+void strrev(char *, char *);
+char *itoa(int, char *, int);
 
 #endif // STRING_H
--- lib/string.c
+++ lib/string.c
@@ -4,7 +4,6 @@
 char *strcpy(char *dest, const char *src)
 {
     char *ret = dest;
-
     if (src == NULL || dest == NULL)
         return NULL;
 
@@ -26,13 +25,13 @@ size_t strlen(const char *str)
     return str_size;
 }
 
-char *strncpy(char *dest, char *src, size_t dest_size)
+char *strncpy(char *dest, const char *src, size_t dest_size)
 {
     size_t i;
     if (src == NULL || dest == NULL)
         return NULL;
 
-    for (i = 0; i < dest_size || *src == NULL; i++) {
+    for (i = 0; i < dest_size || src[i] != '\0'; i++) {
         dest[i] = src[i];
     }
 
@@ -41,5 +40,44 @@ char *strncpy(char *dest, char *src, size_t dest_size)
         i++;
     }
 
-    return i;
+    return dest;
+}
+
+void strrev(char *begin, char *end) {
+	
+	char aux;
+    if (begin == NULL || end == NULL)
+        return;
+	
+	while(end > begin)
+		aux = *end, *end-- = *begin, *begin++ = aux;
+}
+
+char* itoa(int value, char *result, int base)
+{
+    // check that the base if valid
+    if (base < 2 || base > 36) {
+        *result = '\0';
+        return result;
+    }
+
+    char* ptr = result, *ptr1 = result, tmp_char;
+    int tmp_value;
+
+    do {
+        tmp_value = value;
+        value /= base;
+        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+    } while (value);
+
+    // Apply negative sign
+    if (tmp_value < 0)
+        *ptr++ = '-';
+    *ptr-- = '\0';
+    while(ptr1 < ptr) {
+        tmp_char = *ptr;
+        *ptr--= *ptr1;
+        *ptr1++ = tmp_char;
+    }
+    return result;
 }
