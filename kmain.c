42dc9f3f6063479021e990500231184b0d26b014
Ákos Kovács <akoskovacs@gmx.com>
2012-03-04 21:06:03 +0100

    Just some kprintf() tests.

--- kmain.c
+++ kmain.c
@@ -8,6 +8,5 @@ void kmain(void* mbd __unused, unsigned int magic)
        kprint("Ooops, something went wrong! Terminated!n\n");
    }
    init_console();
-///   kprintf("Booting Akosix...\n");
-   kprintf("This is a test of %s with the answer of %d with a hex is %x\n", "Akosix", 42, 0xFFAACC00);
+   kprintf("Booting Akosix...\nAt kmain() %p\n", &kmain);
 }
