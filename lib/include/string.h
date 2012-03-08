c2b99f7031a1fceba11a95e9f23c067236101105
Ákos Kovács <akoskovacs@gmx.com>
2012-03-08 15:19:33 +0100

    Several new string function added:
    strcat() strncat() strstr() strcmp() memcpy() strncpy() strindex()
    memcpy() memclr() memmove() memset()

--- lib/include/string.h
+++ lib/include/string.h
@@ -3,10 +3,20 @@
 
 #include <types.h>
 
-char *strcpy(char *, const char *);
-size_t strlen(const char *);
-char *strncpy(char *, const char *, size_t);
-void strrev(char *, char *);
 char *itoa(int, char *, int);
+char *strcat(char *, const char *);
+char *strncat(char *, const char *, size_t);
+char *strcpy(char *, const char *);
+char *strncpy(char *, const char *, size_t);
+char *strstr(const char *, const char *);
+int memcmp(const void *, const void *, size_t);
+int strcmp(const char *, const char *);
+int strindex(const char *, char);
+size_t strlen(const char *);
+void *memclr(void *, size_t);
+void *memcpy(void *, const void *, size_t);
+void *memmove(void *, const void *, size_t);
+void *memset(void *, int, size_t);
+void strrev(char *, char *);
 
 #endif // STRING_H
--- lib/string.c
+++ lib/string.c
@@ -7,7 +7,7 @@ char *strcpy(char *dest, const char *src)
     if (src == NULL || dest == NULL)
         return NULL;
 
-    while ((*dest++ = *src++))
+    while ((*dest++ = *src++) != '\0')
         ;
 
     return ret;
@@ -43,14 +43,144 @@ char *strncpy(char *dest, const char *src, size_t dest_size)
     return dest;
 }
 
+int strcmp(const char *f, const char *s)
+{
+    while (*f || *s) {
+        if (*f == *s)
+            continue;
+        else if(*f < *s)
+            return -1;
+        else 
+            return 1;
+
+        f++;
+        s++;
+    }
+    return 0;
+}
+
+char *strcat(char *dest, const char *src)
+{
+    char *tmp = dest;
+
+    while (*dest)
+        dest++;
+    while ((*dest++ = *src++) != '\0')
+        ;
+    return tmp;
+}
+
+char *strncat(char *dest, const char *src, size_t count)
+{
+    char *tmp = dest;
+
+    if (count) {
+        while (*dest)
+            dest++;
+        while ((*dest++ = *src++) != 0) {
+            if (--count == 0) {
+                *dest = '\0';
+                break;
+            }
+        }
+    }
+    return tmp;
+}
+
+int strindex(const char *str, char ch)
+{
+    int idx;
+    for (idx = 0; *str != '\0'; idx++) {
+        if (str[idx] == ch)
+            return idx;
+    }
+
+    return -1;
+}
+
+int memcmp(const void *cs, const void *ct, size_t count)
+{
+	const unsigned char *su1, *su2;
+	int res = 0;
+
+	for (su1 = cs, su2 = ct; 0 < count; ++su1, ++su2, count--)
+		if ((res = *su1 - *su2) != 0)
+			break;
+	return res;
+}
+
+char *strstr(const char *s1, const char *s2)
+{
+    size_t l1, l2;
+
+    l2 = strlen(s2);
+    if (!l2)
+        return (char *)s1;
+    l1 = strlen(s1);
+    while (l1 >= l2) {
+        l1--;
+        if (!memcmp(s1, s2, l2))
+            return (char *)s1;
+        s1++;
+    }
+    return NULL;
+}
+
 void strrev(char *begin, char *end) {
-	
-	char aux;
+    
+    char aux;
     if (begin == NULL || end == NULL)
         return;
-	
-	while(end > begin)
-		aux = *end, *end-- = *begin, *begin++ = aux;
+    
+    while(end > begin)
+        aux = *end, *end-- = *begin, *begin++ = aux;
+}
+
+void *memset(void *data, int c, size_t count)
+{
+    char *d = data;
+
+    while (count--)
+        *d++ = c;
+
+    return data;
+}
+
+void *memclr(void *data, size_t size)
+{
+    return memset(data, 0, size);
+}
+
+void *memcpy(void *dest, const void *src, size_t count)
+{
+    char *d = dest;
+    const char *s = src;
+
+    while (count--)
+        *d++ = *s++;
+
+    return dest;
+}
+
+void *memmove(void *dest, const void *src, size_t count)
+{
+    char *tmp;
+    const char *s;
+
+    if (dest <= src) {
+        tmp = dest;
+        s = src;
+        while (count--)
+            *tmp++ = *s++;
+    } else {
+        tmp = dest;
+        tmp += count;
+        s = src;
+        s += count;
+        while (count--)
+            *--tmp = *--s;
+    }
+    return dest;
 }
 
 char* itoa(int value, char *result, int base)
@@ -70,6 +200,12 @@ char* itoa(int value, char *result, int base)
         *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
     } while (value);
 
+    /* To be cool, we need the 0x at the start */
+    if (base == 16) {
+        *ptr++ = 'x';
+        *ptr++ = '0';
+    }
+    
     // Apply negative sign
     if (tmp_value < 0)
         *ptr++ = '-';
