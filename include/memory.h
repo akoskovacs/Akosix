eee39d8cf30a2400c456e925c8d3331ac9281b1a
Ákos Kovács <akoskovacs@gmx.com>
2012-03-09 21:32:10 +0100

    Multiboot structures deleted

--- include/memory.h
+++ include/memory.h
@@ -3,13 +3,4 @@
 
 #include <types.h>
 
-struct multiboot_memory_map {
-    uint32_t size;
-    uint32_t base_low_addr;
-    uint32_t base_high_addr;
-    uint32_t low_length;
-    uint32_t high_length;
-    uint32_t type;
-};
-
 #endif // MEMORY_H
