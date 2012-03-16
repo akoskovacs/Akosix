9979ef7044b31f38f897435285e0b3aca5701e4d
Ákos Kovács <akoskovacs@gmx.com>
2012-03-16 01:19:33 +0100

    Added lower-half page setup code

---
+++ boot/pgsetup.c
@@ -0,0 +1,27 @@
+#define __SETUP__
+#include <page.h>
+
+uint32_t page_directory[1024] __setup_data __align(PAGE_SIZE);
+uint32_t page_table[1024] __setup_data __align(PAGE_SIZE);
+void     setup_paging(void) __setup;
+
+void setup_paging(void)
+{
+    register int i;
+    for (i = 0; i < 1024; i++) {
+        page_directory[i] = 0;
+        page_table[i] = 0;
+        /* Map the first 4MB and set the appropriate flags */
+        page_table[i] = (i * PAGE_SIZE) | PT_PRESENT | PT_RW;
+    }
+    
+    page_directory[0] = (uint32_t)&page_table | PD_RW | PD_PRESENT;
+    page_directory[768] = (uint32_t)&page_table | PD_RW | PD_PRESENT;
+    __asm__ __volatile__("movl %0, %%cr3\n"
+                         "movl %%cr0, %%eax\n"
+                         "orl 0x80000000, %%eax\n"
+                         "movl %%eax, %%cr0\n"
+                         : /* No output */
+                         : "b"((uint32_t)&page_directory)
+                         : "eax");
+}
--- include/page.h
+++ include/page.h
@@ -5,7 +5,7 @@
 #include <basic.h>
 #define PAGE_SIZE 4096
 
-struct page {
+struct page_entry {
     uint32_t present  : 1;
     uint32_t rw       : 1;
     uint32_t user     : 1;
@@ -15,7 +15,33 @@ struct page {
     uint32_t frame    : 20;
 };
 
-struct page page_table[1024] __align(PAGE_SIZE);
-struct page page_directory[1024] __align(PAGE_SIZE);
+/* Bit flags for page-directory entries */
+enum PDE_FLAGS {
+    PD_PRESENT       = 0x01,
+    PD_RW            = 0x02,
+    PD_USER          = 0x04,
+    PD_PWT           = 0x08, /* Page level write-through */
+    PD_CACHE_DISABLE = 0x10,
+    PD_ACESSED       = 0x20,
+    /* 6th bit ignored */
+    PD_4MB_PAGES     = 0x80
+    /* 8th bit ignored */
+    /* 12-32 physical address of page table */
+};
+
+/* Bit flags for page-table entries */
+enum PTE_FLAGS {
+    PT_PRESENT       = 0x01,
+    PT_RW            = 0x02,
+    PT_USER          = 0x04,
+    PT_PWT           = 0x08, /* Page level write-through */
+    PT_CACHE_DISABLE = 0x10,
+    PT_ACESSED       = 0x20,
+    PT_DIRTY         = 0x40,
+    PT_PAT           = 0x40,
+    PT_GLOBAL        = 0x80  /* Global translation */
+    /* 11-9 ignored */
+    /* 12-32 physical address of the page */
+};
 
 #endif // PAGE_H
