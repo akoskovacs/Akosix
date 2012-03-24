3034c177a2f90c9b48a0f70a1acaa88d4dc248c9
Ákos Kovács <akoskovacs@gmx.com>
2012-03-24 14:43:16 +0100

    Corrected type definitions

--- include/types.h
+++ include/types.h
@@ -1,20 +1,24 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-typedef unsigned int size_t;
-typedef unsigned long off_t;
-typedef unsigned int paddr_t;
+#ifdef __x86_64__
+#error "Sorry, but you cannot compile Akosix in a 64-bit environment, yet. :-("
+#endif
+
+typedef unsigned long size_t;
+typedef unsigned long long off_t;
+typedef unsigned long paddr_t;
 typedef void * vaddr_t;
 
 typedef unsigned char uint8_t;
 typedef unsigned short int uint16_t;
 typedef unsigned int uint32_t; 
-typedef unsigned long uint64_t; 
+typedef unsigned long long uint64_t; 
 
 typedef signed char int8_t;
 typedef signed short int int16_t;
 typedef signed int int32_t;
-typedef signed long int64_t; 
+typedef signed long long int64_t; 
 
 typedef enum { false, true } bool;
 
