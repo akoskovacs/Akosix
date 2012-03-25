360730fd28036cea4f71ccdb0a5a098f16d57c0f
Ákos Kovács <akoskovacs@gmx.com>
2012-03-25 22:23:53 +0200

    kprintf(): Better formatting with itoa() and uitoa()

--- lib/kprintf.c
+++ lib/kprintf.c
@@ -10,8 +10,8 @@ int vsnprintf(char *dest, size_t size, const char *fmt, va_list ap)
    char *tmp;
    size_t asize = 0;
    size_t nsize = 0;
-   char buffer[20];
-   int num;
+   char buffer[30];
+   unsigned int unum = 0;
 
    while (*fmt != '\0') {
        if (*fmt == '%') {
@@ -23,23 +23,32 @@ int vsnprintf(char *dest, size_t size, const char *fmt, va_list ap)
                    strncpy(dest+asize, tmp, size-asize);
                    asize += nsize;
                break;
-               
+
+               case 'u':
+                   uitoa(va_arg(ap, unsigned int), 10, buffer);
+                   nsize = strlen(buffer);
+                   strncpy(dest+asize, buffer, size-asize);
+                   asize += nsize;
+               break;
+
                case 'd': case 'i':
-                   tmp = itoa(va_arg(ap, int), buffer, 10);
-                   nsize = strlen(tmp);
-                   strncpy(dest+asize, tmp, size-asize);
+                   itoa(va_arg(ap, int), 10, buffer);
+                   nsize = strlen(buffer);
+                   strncpy(dest+asize, buffer, size-asize);
                    asize += nsize;
                break;
 
                case 'x': case 'p':
                     if (*fmt == 'x')
-                        num = va_arg(ap, int);
+                        unum = va_arg(ap, unsigned int);
                     else 
-                        num = (unsigned int)va_arg(ap, void *);
+                        unum = (unsigned int)va_arg(ap, void *);
 
-                    tmp = itoa(num, buffer, 16);
-                    nsize = strlen(tmp);
-                    strncpy(dest+asize, tmp, size-asize);
+                    strncpy(dest-asize, "0x", size-asize);
+                    asize += 2;
+                    uitoa(unum, 16, buffer);
+                    nsize = strlen(buffer);
+                    strncpy(dest+asize, buffer, size-asize);
                     asize += nsize;
                break; 
 
