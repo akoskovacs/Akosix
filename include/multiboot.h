3c018047389e51ca6e2e137910e05156dbf38ee2
Ákos Kovács <akoskovacs@gmx.com>
2012-03-25 22:24:52 +0200

    Multiboot flags added

--- include/multiboot.h
+++ include/multiboot.h
@@ -1,21 +1,37 @@
 #ifndef MULTIBOOT_H
 #define MULTIBOOT_H
 
-struct multiboot_elf_section {
+#include <basic.h>
+
+enum multiboot_flags {
+    MB_INFO_MEMORY        = 0x0001,
+    MB_INFO_BOOTDEV       = 0x0002,
+    MB_INFO_CMDLINE       = 0x0004,
+    MB_INFO_MODS          = 0x0008,
+    MB_INFO_ELF_SECT      = 0x0020,
+    MB_INFO_MEM_MAP       = 0x0040,
+    MB_INFO_DRIVE_INFO    = 0x0080,
+    MB_INFO_CONFIG_TABLE  = 0x0100,
+    MB_INFO_BOOT_LOADER   = 0x0200,
+    MB_INFO_APM_TABLE     = 0x0400,
+    MB_INFO_VIDEO         = 0x0800
+};
+
+struct __packed multiboot_elf_section {
     uint32_t num;
     uint32_t size;
     uint32_t addr;
     uint32_t shndx;
-} __packed;
+};
 
-struct multiboot_mmap_entry {
+struct __packed multiboot_mmap_entry {
     uint32_t size;
     uint64_t addr;
     uint64_t length;
 #define MB_MEMORY_AVAILABLE 1
 #define MB_MEMORY_RESERVED 2
     uint32_t type;
-} __packed;
+};
 
 struct multiboot_info {
     uint32_t flags;
@@ -25,9 +41,9 @@ struct multiboot_info {
     uint32_t cmdline;
     uint32_t mods_count;
     uint32_t mods_addr;
-    struct multiboot_elf_section elf_sec;
+    struct   multiboot_elf_section elf_sec;
     uint32_t mmap_length;
-    struct multiboot_mmap_entry *mmap_addr;
+    uint32_t mmap_addr;
     uint32_t drives_length;
     uint32_t drivers_addr;
     uint32_t config_table;
