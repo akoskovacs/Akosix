c76c45471937f2c406d1da326ac9e3707f60f62d
Ákos Kovács <akoskovacs@gmx.com>
2012-03-30 18:53:54 +0200

    kmain(): Added some multiboot feature detection

--- kmain.c
+++ kmain.c
@@ -3,49 +3,45 @@
 #include <system.h>
 #include <string.h>
 #include <multiboot.h>
-#include <page.h>
+#include <memory.h>
 
 extern char __end_data_kernel;
 
-void kmain(struct multiboot_info *mbi __unused, unsigned int magic)
+void kmain(struct multiboot_info *mbi, unsigned int magic)
 {
    unsigned int mem = mbi->mem_upper + mbi->mem_lower;
+
    set_console_attributes(FG_COLOR_WHITE | LIGHT | BG_COLOR_BLUE);
    init_console();
+
    if (magic != 0x2BADB002) {
        kprint("Ooops, something went wrong! Terminated!n\n");
    }
+
    kprintf("Booting Akosix...\nAt kmain() %p\n", kmain);
-   kprintf("Memory present %dKB.\n", mem);
-   kprintf("command line: %s\n", (const char *)mbi->cmdline);
+   kprintf("Memory present %dKB (%dMB).\n", mem, mem/1024);
+   kprintf("Bootloader: %s\n", (const char *)mbi->boot_loader_name);
+   kprintf("Command line: %s\n", (const char *)mbi->cmdline);
    kprintf("End of kernel %p\n", (void*)&__end_data_kernel);
-   kprintf("Paging enabled!\n");
+   kprintf("Paging enabled!\n\n");
    kprintf("Multiboot info:\n");
+
    if (mbi->flags & MB_INFO_BOOTDEV) 
-       kprintf("\tHas multiboot bootdev\n");
+       kprintf("\to Has multiboot bootdev\n");
    if (mbi->flags & MB_INFO_CMDLINE) 
-       kprintf("\tHas multiboot command line\n");
+       kprintf("\to Has multiboot command line\n");
    if (mbi->flags & MB_INFO_ELF_SECT) 
-       kprintf("\tHas multiboot elf section info\n");
+       kprintf("\to Has multiboot elf section info\n");
    if (mbi->flags & MB_INFO_DRIVE_INFO) 
-       kprintf("\tHas multiboot drive info\n");
+       kprintf("\to Has multiboot drive info\n");
    if (mbi->flags & MB_INFO_VIDEO) 
-       kprintf("\tHas multiboot video info\n");
+       kprintf("\to Has multiboot video info\n");
    if (mbi->flags & MB_INFO_MODS) 
-       kprintf("\tHas multiboot module info\n");
-   if (mbi->flags & MB_INFO_MEM_MAP) {
-       kprintf("\tHas multiboot memory-map info:\n");
-       unsigned int i = 0;
-       struct multiboot_mmap_entry *mmap;
-       mmap = (struct multiboot_mmap_entry *)mbi->mmap_addr;
-       while (i < mbi->mmap_length) {
-           mmap = (struct multiboot_mmap_entry *)mbi->mmap_addr+i;
-           kprintf("\t\tsize %d, addr %x, length %d [%savailable]\n"
-           , mmap->size, mmap->addr
-           , ((mmap->type == MB_MEMORY_AVAILABLE) ? "" : "not "));
-           i += sizeof(struct multiboot_mmap_entry);
-       }
-   }
+       kprintf("\to Has multiboot module info\n");
    if (mbi->flags & MB_INFO_MEMORY) 
-       kprintf("\tHas multiboot memory\n\n");
+       kprintf("\to Has multiboot memory\n");
+   if (mbi->flags & MB_INFO_MEM_MAP)
+       kprintf("\to Has multiboot memory-map info:\n");
+
+   init_memory(mbi);
 }
