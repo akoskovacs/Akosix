419b88da92b31d8e715889d260bee5729d7623c8
Ákos Kovács <akoskovacs@gmx.com>
2012-04-06 01:45:53 +0200

    page.h: A bunch of page manipulating macros added

--- include/page.h
+++ include/page.h
@@ -4,15 +4,27 @@
 #include <types.h>
 #include <basic.h>
 
-struct page_entry {
-    uint32_t present  : 1;
-    uint32_t rw       : 1;
-    uint32_t user     : 1;
-    uint32_t accessed : 1;
-    uint32_t dirty    : 1;
-    uint32_t unused   : 7;
-    uint32_t frame    : 20;
-};
+#define IS_PAGE_PRESENT(p) (((p) & PT_PRESENT)?true:false)
+#define IS_PAGE_WRITEABLE(p) (((p) & PT_RW)?true : false)
+#define IS_PAGE_RO(p) !IS_PAGE_WRITEABLE(p)
+#define IS_PAGE_USER(p) (((p) & PT_USER)?true:false)
+#define IS_PAGE_SYSTEM(p) !IS_PAGE_USER(p)
+#define IS_PAGE_DIRTY(p) (((p) & PT_DIRTY)?true:false)
+#define IS_PAGE_WRITE_THROUGH(p) (((p) & PT_CACHE_DISABLE)?true:false)
+#define IS_PAGE_WRITE_BACK(p) !IS_PAGE_WRITE_THROUGH(p)
+#define PAGE_FRAME(p) ((p) >> PAGE_SHIFT)
+
+#define SET_PAGE_FRAME(p, f) (p |= ((f) << PAGE_SHIFT))
+#define SET_PAGE_PRESENT(p) ((p) |= PT_PRESENT)
+#define SET_PAGE_WRITEABLE(p) ((p) |= PT_RW)
+#define SET_PAGE_RO(p) ((p) &= PT_RW)
+#define SET_PAGE_USER(p) ((p) |= PT_USER)
+#define SET_PAGE_SYSTEM(p) ((p) &= PT_USER)
+#define SET_PAGE_DIRTY(p) ((p) |= PT_USER)
+
+#define CLEAR_PAGE_PRESENT(p) ((p) &= PT_PRESENT)
+#define CLEAR_PAGE_WRITEABLE(p) ((p) &= PT_RW)
+#define CLEAR_PAGE_DIRTY(p) ((p) &= PT_DIRTY)
 
 /* Bit flags for page-directory entries */
 enum PDE_FLAGS {
@@ -37,10 +49,12 @@ enum PTE_FLAGS {
     PT_CACHE_DISABLE = 0x10,
     PT_ACESSED       = 0x20,
     PT_DIRTY         = 0x40,
-    PT_PAT           = 0x40,
-    PT_GLOBAL        = 0x80  /* Global translation */
+    PT_PAT           = 0x80,
+    PT_GLOBAL        = 0x100  /* Global translation */
     /* 11-9 ignored */
     /* 12-32 physical address of the page */
 };
+
+typedef enum PTE_FLAGS page_flags_t;
 
 #endif // PAGE_H
