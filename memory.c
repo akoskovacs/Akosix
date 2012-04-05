464c770ffcef1141916c821308534948451735a1
Ákos Kovács <akoskovacs@gmx.com>
2012-04-06 01:49:05 +0200

    memory.c: Call the new physical manager

--- memory.c
+++ memory.c
@@ -3,21 +3,13 @@
 #include <memory.h>
 #include <kernel.h>
 
-static void *expand_kheap(size_t size);
-static void *contract_kheap(size_t size);
-static vaddr_t kheap_start;
-static vaddr_t kheap_end;
-static vaddr_t kheap_max;
-static uint8_t __end_data_kernel;
-
-void init_memory(struct multiboot_info *mbi)
+void memory_init(struct multiboot_info *mbi)
 {
     /* Make it shorter */
     typedef struct multiboot_mmap_entry mb_mmap_t;
 
     uint64_t length_addr;
     unsigned int size;
-    long long addr;
     const char *avail = NULL;
     mb_mmap_t *mmap = (mb_mmap_t *)mbi->mmap_addr;
     kheap_start = &__end_data_kernel;
@@ -27,28 +19,24 @@ void init_memory(struct multiboot_info *mbi)
     }
 
     while (mmap < (mb_mmap_t *)((uint32_t)mbi->mmap_addr + mbi->mmap_length)) {
-        switch (mmap->type) {
-            case MB_MEMORY_RESERVED:
-                avail = "reserved";
-            break;
-            case MB_MEMORY_AVAILABLE:
-                avail = "available";
-            break;
-        }
         length_addr = mmap->addr + mmap->length;
         size = mmap->length/1024;
-        
         kprintf("\t\t- 0x%X%X - 0x%X%X, size: %dKB [%s]\n"
-        , mmap->addr
-        , length_addr
-        , size
-        , avail);
+        , mmap->addr, length_addr, size
+        , (mmap->type == MB_MEMORY_RESERVED) ? "reserved" : "available");
+
+        if (mmap->addr == 0x100000 
+                && mmap->type == MB_MEMORY_AVAILABLE) {
+            pmm_init(mmap->addr, mmap->length);
+            kheap_max = VADDR((uint32_t)length_addr);
+        }
+
         mmap = (mb_mmap_t *)((uint32_t)mmap
             + mmap->size + sizeof(mmap->size));
     }
 }
 
-static void *expand_kheap(size_t size)
+void *expand_kheap(size_t size)
 {
     void *alloc = NULL;
     if (kheap_end + size > kheap_max) {
@@ -59,7 +47,7 @@ static void *expand_kheap(size_t size)
     return alloc;
 }
 
-static void *contract_kheap(size_t size)
+void *contract_kheap(size_t size)
 {
     if (kheap_end - size < kheap_start) {
         return NULL;
