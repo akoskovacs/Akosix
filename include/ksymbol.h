8cf87eef36037d0d5ce9225a7db53d3c83ecfcec
Ákos Kovács <akoskovacs@gmx.com>
2012-04-20 21:41:21 +0200

    ksymbol.h: Added

---
+++ include/ksymbol.h
@@ -0,0 +1,25 @@
+#ifndef KSYMBOL_H
+#define KSYMBOL_H
+
+#include <types.h>
+
+typedef enum {
+    SYM_BSS  = 0x001,
+    SYM_DATA = 0x002,
+    SYM_CODE = 0x004,
+    SYM_ANY = SYM_BSS | SYM_CODE | SYM_DATA,
+    SYM_VARIABLE = SYM_BSS | SYM_DATA,
+    SYM_FUNCTION = SYM_CODE
+} symbol_type_t;
+
+struct ksymbol {
+    uint32_t      ks_address;
+    symbol_type_t ks_type;
+    const char   *ks_name;
+};
+
+void *get_ksymbol(const char *, symbol_type_t);
+#define get_kfunction(name) get_ksymbol((name), SYM_CODE)
+#define get_kvar(name) get_ksymbol((name), SYM_VARIABLE)
+
+#endif /* KSYMBOL_H */
