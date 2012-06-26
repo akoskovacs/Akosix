26e48cc6a9bf820293bf7a000f6012212dacedbe
Ákos Kovács <akoskovacs@gmx.com>
2012-06-26 03:17:21 +0200

    __panic(): Modified to work with panic.h

--- panic.c
+++ panic.c
@@ -14,15 +14,16 @@
  * included in all copies or substantial portions of the Software.
  * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
- *************************************************************************/
+ ************************************************************************/
 #include <types.h>
 #include <basic.h>
 #include <kernel.h>
+#include <panic.h>
 #include <console.h>
 #include <string.h>
 #include <config.h>
 
-#define BSIZE    2*CONFIG_CONSOLE_WIDTH
+#define BSIZE    2 * CONFIG_CONSOLE_WIDTH
 
 void __panic(struct x86_registers regs, const char *fmt, ...)
 {
@@ -31,7 +32,9 @@ void __panic(struct x86_registers regs, const char *fmt, ...)
     size_t panic_str_len = strlen(panic_str);
     size_t line_len = 0;
     va_list ap;
-    int x, y;
+    int x, y, i;
+    union x86_regs_u r;
+    r.s_reg = regs;
     console_attr_t basic_attrs = BG_COLOR_RED | FG_COLOR_WHITE;
     set_console_attributes(basic_attrs);
     clear_console(); 
@@ -45,25 +48,18 @@ void __panic(struct x86_registers regs, const char *fmt, ...)
         kpos_print((CONFIG_CONSOLE_WIDTH-line_len)/2, 3, buffer);
     }
     set_xy(0, 5);
-    kprintf("\tEAX: %d [%x]\n", regs.eax, regs.eax);
-    kprintf("\tEBX: %d [%x]\n", regs.ebx, regs.ebx);
-    kprintf("\tECX: %d [%x]\n", regs.ecx, regs.ecx);
-    kprintf("\tEDX: %d [%x]\n", regs.edx, regs.edx);
-    kprintf("\tESP: %d [%x]\n", regs.esp, regs.esp);
-    kprintf("\tEBP: %d [%x]\n", regs.ebp, regs.ebp);
-    kprintf("\tEDI: %d [%x]\n", regs.edi, regs.edi);
-    kprintf("\tESI: %d [%x]\n\n", regs.esi, regs.esi);
-    kprintf("\tCS: %d [%x]\n", regs.cs, regs.cs);
-    kprintf("\tDS: %d [%x]\n", regs.ds, regs.ds);
-    kprintf("\tSS: %d [%x]\n", regs.ss, regs.ss);
-    kprintf("\tES: %d [%x]\n", regs.es, regs.es);
-    kprintf("\tFS: %d [%x]\n", regs.fs, regs.fs);
-    kprintf("\tGS: %d [%x]\n\n", regs.gs, regs.gs);
-#if 0
-    kprintf("\tCR0: %d [%x]\n", regs.cr0, regs.cr0);
-    kprintf("\tCR1: %d [%x]\n", regs.cr1, regs.cr1);
-    kprintf("\tCR2: %d [%x]\n", regs.cr2, regs.cr2);
-    kprintf("\tCR4: %d [%x]\n", regs.cr3, regs.cr3);
-#endif
+    for (i = 0; i <= X86_REG_EDI; i++) {
+        kprintf("\t%s: %d [%x]\n", x86_register_name[i], r.a_reg[i], r.a_reg[i]);
+    }
+    kprintf("\n");
+    /* Segment registers are 16 bit long. Split the 32 bit array 
+      elements to high and low variables to overcome this. */
+    for (i = X86_REG_SS; i <= X86_REG_GS; i++) {
+        uint16_t high, low;
+        high = r.a_reg[i] >> 16;
+        low = r.a_reg[i] & 0xFFFF;
+        kprintf("\t%s: %d [%x]\n", x86_register_name[i], high, high);
+        kprintf("\t%s: %d [%x]\n", x86_register_name[++i], low, low);
+    }
     hang();
 }
