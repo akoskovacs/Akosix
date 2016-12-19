107f818c996f35542a57578d67edbf995f06233f
Ákos Kovács <akoskovacs@gmx.com>
2016-12-20 00:30:42 +0100

    system.h: Make bool one byte long

--- include/types.h
+++ include/types.h
@@ -9,11 +9,11 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
@@ -25,6 +25,7 @@
 #define TYPES_H
 
 #include <config.h>
+#include <basic.h>
 
 typedef unsigned long size_t;
 typedef unsigned long long off_t;
@@ -34,14 +35,14 @@ typedef unsigned long vaddr_t;
 
 typedef unsigned char uint8_t;
 typedef unsigned short int uint16_t;
-typedef unsigned int uint32_t; 
-typedef unsigned long long uint64_t; 
+typedef unsigned long uint32_t;
+typedef unsigned long long uint64_t;
 
 typedef signed char int8_t;
 typedef signed short int int16_t;
-typedef signed int int32_t;
-typedef signed long long int64_t; 
+typedef signed long int32_t;
+typedef signed long long int64_t;
 
-typedef enum { false, true } bool;
+typedef enum __packed { false, true } bool;
 
 #endif // TYPES_H
